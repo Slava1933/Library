199ceb8e1e9733f1a43cc8600b60fff343f1bbd2
Slava <slavik100tanki@gmail.com>
2026-03-13 18:08:52 +0700

    added handlers, 1 repository func, fixed some things

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,114 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"library/internal/interfaces"
+	"library/internal/models"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"go.uber.org/zap"
+)
+
+type Handlers struct {
+	repo interfaces.Repository_interface
+	log  *zap.Logger
+}
+
+func (h *Handlers) GetDisciplinesHandler(w http.ResponseWriter, r *http.Request) {
+	disciplines, err := h.repo.FindAllDisciplines(r.Context())
+	if err != nil {
+		h.log.Error("Cant get the disciplines", zap.Error(err))
+		http.Error(w, "Cant get the disciplines from server", http.StatusInternalServerError)
+		return
+	}
+
+	if disciplines == nil {
+		disciplines = []models.Discipline{}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(disciplines); err != nil {
+		h.log.Error("Failed to encode disciplines to json", zap.Error(err))
+		http.Error(w, "Failed to encode disciplines to json", http.StatusInternalServerError)
+	}
+	h.log.Info("Get disciplines was successfully ended", zap.Int("count:", len(disciplines)))
+}
+
+func (h *Handlers) GetDocsByDiscipline(w http.ResponseWriter, r *http.Request) {
+	pathparts := strings.Split(r.URL.Path, "/")
+
+	if len(pathparts) < 4 {
+		h.log.Error("Bad request, small path")
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	id := pathparts[3]
+	DisciplineID, err := strconv.Atoi(id)
+	if err != nil {
+		h.log.Error("Cant convert DisciplineID to integer", zap.Error(err))
+		http.Error(w, "Cant convert DisciplineID to integer", http.StatusInternalServerError)
+		return
+	}
+
+	Documents, err := h.repo.FindDocumentsByDiscipline(r.Context(), DisciplineID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			h.log.Info("Documents not found", zap.Int("Discipline id", DisciplineID))
+			http.Error(w, "Documents not found", http.StatusNotFound)
+			return
+		}
+		h.log.Error("Cant get the documents of discipline",
+			zap.Int("discipline id:", DisciplineID), zap.Error(err))
+		http.Error(w, "Cant get the documents from server", http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(Documents); err != nil {
+		h.log.Error("Failed to encode documents to json", zap.Error(err))
+		http.Error(w, "Failed to encode documents to json", http.StatusInternalServerError)
+	}
+	h.log.Info("Get documents was succcessfully ended", zap.Int("discipline:", DisciplineID),
+		zap.Int("counts", len(Documents)))
+}
+
+func (h *Handlers) GetDocument(w http.ResponseWriter, r *http.Request) {
+	pathparts := strings.Split(r.URL.Path, "/")
+
+	if len(pathparts) < 4 {
+		h.log.Error("Bad request, small path")
+		http.Error(w, "Invaild URL", http.StatusBadRequest)
+		return
+	}
+
+	id := pathparts[3]
+	DocumentID, err := strconv.Atoi(id)
+	if err != nil {
+		h.log.Error("Cant convert DocumentID to integer", zap.Error(err))
+		http.Error(w, "Cant convert DocumenID to integer", http.StatusInternalServerError)
+		return
+	}
+	Document, err := h.repo.FindDocument(r.Context(), DocumentID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			h.log.Info("Document not found", zap.Int("id", DocumentID))
+			http.Error(w, "Document not found", http.StatusNotFound)
+			return
+		}
+		h.log.Error("Cant get document to download", zap.Int("Document ID", DocumentID),
+			zap.Error(err))
+		http.Error(w, "Cant get the document to download from server", http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err = json.NewEncoder(w).Encode(Document); err != nil {
+		h.log.Error("Failed to encode document to json", zap.Error(err))
+		http.Error(w, "Failed to encode document to json", http.StatusInternalServerError)
+	}
+	h.log.Info("Get document to download was successfully ended", zap.Int("Document ID", DocumentID))
+}
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,13 +1,13 @@
 package models
 
 type Discipline struct {
-	ID    int64  `db:"id" json:"id"`
+	ID    int    `db:"id" json:"id"`
 	Title string `db:"title" json:"title"`
 }
 
 type Document struct {
-	ID           int64  `db:"id" json:"id"`
-	DisciplineID int64  `db:"discipline_id" json:"discipline_id"`
+	ID           int    `db:"id" json:"id"`
+	DisciplineID int    `db:"discipline_id" json:"discipline_id"`
 	Title        string `db:"title" json:"title"`
 	Filepath     string `db:"file_path" json:"file_path"`
 }
